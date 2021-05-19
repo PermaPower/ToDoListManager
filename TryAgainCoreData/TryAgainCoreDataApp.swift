68e760caced9abd530a4ebba5773c1d468c83bc7
David Power <dpower19@gmail.com>
2021-05-19 20:54:29 +1000

    Initial Commit

---
+++ TryAgainCoreData/TryAgainCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryAgainCoreDataApp.swift
+//  TryAgainCoreData
+//
+//  Created by David Power on 19/5/21.
+//
+
+import SwiftUI
+
+@main
+struct TryAgainCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
